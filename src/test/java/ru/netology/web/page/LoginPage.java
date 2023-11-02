4635fb7181328f0ef2aec875504a1f5481de2589
Neto.Changes <infero-sky@yandex.ru>
2023-11-03 01:06:47 +0300

    Final commit

---
+++ src/test/java/ru/netology/web/page/LoginPage.java
@@ -0,0 +1,19 @@
+package ru.netology.web.page;
+
+import com.codeborne.selenide.SelenideElement;
+import ru.netology.web.data.DataHelper;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class LoginPage {
+    private static final SelenideElement loginField = $("[data-test-id=login] input");
+    private static final SelenideElement passwordField = $("[data-test-id=password] input");
+    private static final SelenideElement loginButton = $("[data-test-id=action-login");
+
+    public static VerificationPage validLogin(DataHelper.AuthInfo info) {
+        loginField.setValue(info.getLogin());
+        passwordField.setValue(info.getPassword());
+        loginButton.click();
+        return new VerificationPage();
+    }
+}
